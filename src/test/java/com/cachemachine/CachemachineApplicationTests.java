5fa0b478f1b7693ff41387fecba306c274816925
slava <zavgorodniy@gmail.com>
2021-12-08 19:42:27 +0200

    initial commit: add spring boot to project

---
+++ src/test/java/com/cachemachine/CachemachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cachemachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachemachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
